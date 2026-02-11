a2f517aca570261d8c5f1c7c2ce5b8d8739beb03
hajer <hajer@Hajer-PC>
2026-02-11 22:56:13 +0400

    Added headless Chrome mode for CI

--- AutomatedTestingSQA/src/test/java/tests/BaseTest.java
+++ AutomatedTestingSQA/src/test/java/tests/BaseTest.java
@@ -1,33 +1,20 @@
-package tests;
+@BeforeEach
+public void setUp() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.time.Duration;
-
-public class BaseTest {
-    protected WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        // Set up ChromeDriver automatically
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
+    String ciEnv = System.getenv("CI");
+    if ("true".equalsIgnoreCase(ciEnv)) {
+      
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+    } else {
+       
         options.addArguments("--start-maximized");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+    driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 }
