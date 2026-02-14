744fb9351d0a88f2a944b618c585e0ffdadd4594
hajer <hajer@Hajer-PC>
2026-02-15 00:54:21 +0400

    Fix: enable headless Chrome mode for GitHub CI

--- AutomatedTestingSQA/src/test/java/tests/BaseTest.java
+++ AutomatedTestingSQA/src/test/java/tests/BaseTest.java
@@ -1,20 +1,33 @@
-@BeforeEach
-public void setUp() {
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
 
-    String ciEnv = System.getenv("CI");
-    if ("true".equalsIgnoreCase(ciEnv)) {
-      
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-    } else {
-       
-        options.addArguments("--start-maximized");
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // Detect if running inside GitHub Actions
+        String ci = System.getenv("CI");
+
+        if ("true".equalsIgnoreCase(ci)) {
+            // Run Chrome in headless mode for CI (no GUI)
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        } else {
+            // Normal mode when you run locally in Eclipse
+            options.addArguments("--start-maximized");
+        }
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-
-    driver = new ChromeDriver(options);
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 }
